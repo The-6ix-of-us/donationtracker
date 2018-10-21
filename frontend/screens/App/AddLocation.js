2c63769837c7f0a58a2e7dc94256f38a97d0f9c3
Emily Wilson <emilywilson6263@gmail.com>
2018-10-21 08:48:27 -0400

    add cancel button to addLocation screen

--- frontend/screens/App/AddLocation.js
+++ frontend/screens/App/AddLocation.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, ScrollView } from 'react-native';
+import { View, ScrollView, StyleSheet } from 'react-native';
 import { Button, FormInput, FormLabel } from 'react-native-elements';
 
 import AppHeader from '../../components/AppHeader';
@@ -24,6 +24,7 @@ class AddLocation extends Component {
     };
 
     this.onPressAddLocation = this.onPressAddLocation.bind(this);
+    this.onPressCancel = this.onPressCancel.bind(this);
   }
 
   onPressAddLocation() {
@@ -43,9 +44,13 @@ class AddLocation extends Component {
     this.props.navigation.navigate('Home');
   }
 
+  onPressCancel() {
+    this.props.navigation.navigate('Home');
+  }
+
   render() {
     return (
-      <View>
+      <View style={{ flex: 1 }}>
         <AppHeader {...this.props} />
         <ScrollView>
           <FormLabel>Location Name</FormLabel>
@@ -68,11 +73,22 @@ class AddLocation extends Component {
           <FormInput onChangeText={(e) => this.setState({ website: e })}/>
           <FormLabel>Phone</FormLabel>
           <FormInput onChangeText={(e) => this.setState({ phone: e })}/>
+          <View style={styles.buttonContainer}>
+            <Button title="ADD LOCATION" onPress={this.onPressAddLocation}/>
+            <Button title="CANCEL" onPress={this.onPressCancel}/>
+          </View>
         </ScrollView>
-        <Button title="ADD LOCATION" onPress={this.onPressAddLocation}/>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  buttonContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    flex: 1
+  }
+});
 
 export default AddLocation;
