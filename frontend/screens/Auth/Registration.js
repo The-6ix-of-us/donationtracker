d8e72ce8762dcc32cbac7711565d06402c6d857d
Emily Wilson <emilywilson6263@gmail.com>
2018-10-20 23:39:08 -0400

    add registration dunctionality

--- frontend/screens/Auth/Registration.js
+++ frontend/screens/Auth/Registration.js
@@ -14,26 +14,59 @@ import {
   FormInput
 } from 'react-native-elements';
 
+import firebase, { db } from '../../../firebaseConfig';
+
 class Registration extends Component {
   static navigationOptions = {
     title: 'Register',
   };
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: '',
+      password: '',
+      userType: 'User',
+    }
+
+    this.onPressRegister = this.onPressRegister.bind(this);
+  }
+
+  onPressRegister() {
+    firebase.auth().createUserWithEmailAndPassword(this.state.email, this.state.password)
+    .then((userCredential) => {
+      db.collection('users').doc(userCredential.user.uid).set({
+        email: userCredential.user.email,
+        'user-type': this.state.userType,
+      });
+      this.props.navigation.navigate('Main');
+    })
+    .catch((error) => {
+      // Handle Errors here.
+      this.email_input.shake();
+      console.log(error);
+      var errorCode = error.code;
+      var errorMessage = error.message;
+    });
+  }
+
   render() {
     return (
       <View style={styles.container}>
         <View style={styles.content}>
           {/* <FormLabel>Display Name</FormLabel>
           <FormInput /> */}
-          <FormLabel>Username</FormLabel>
-          <FormInput />
+          <FormLabel>Email</FormLabel>
+          <FormInput onChangeText={e => this.setState({ email: e })} ref={ref => this.email_input = ref}/>
           <FormLabel>Password</FormLabel>
-          <FormInput secureTextEntry={true} />
-          {/* <Picker>
-            <Picker.Item label="Admin" />
-            <Picker.Item label="Location Employee" />
-            <Picker.Item label="User" />
-          </Picker> */}
-          <Button raised title="Register" />
+          <FormInput secureTextEntry={true} onChangeText={e => this.setState({ password: e })}/>
+          <Picker selectedValue={this.state.userType} onValueChange={value => this.setState({ userType: value })}>
+            <Picker.Item label="Admin" value="Admin" />
+            <Picker.Item label="Location Employee" value="Location Employee"/>
+            <Picker.Item label="User" value="User" />
+          </Picker>
+          <Button raised title="Register" onPress={this.onPressRegister} />
           <TouchableOpacity onPress={() => this.props.navigation.navigate('Login')}>
             <FormLabel>Login here</FormLabel>
           </TouchableOpacity>
