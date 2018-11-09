d040aa0b0d5b434099b880e9f5d320e4fafa88d8
nadun <nadun.ranawaka@gatech.edu>
2018-11-08 20:30:29 -0500

    Ran Lint and fixed some issues

--- app/src/main/java/cs2340/donationtracker/model/LocationModel.java
+++ app/src/main/java/cs2340/donationtracker/model/LocationModel.java
@@ -1,16 +1,12 @@
 package cs2340.donationtracker.model;
 
-import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 
-import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 
 import java.util.ArrayList;
 import java.util.List;
-
-import cs2340.donationtracker.controllers.LocationListActivity;
 
 /**
  * A class representing a collection of locations
@@ -18,6 +14,11 @@ import cs2340.donationtracker.controllers.LocationListActivity;
 
 public class LocationModel {
     private static final LocationModel _instance = new LocationModel();
+
+    /**
+     * getter for the current Location Model
+     * @return the instance of the Location Model
+     */
     public static LocationModel getInstance() { return _instance; }
 
     private List<Location> locations;
@@ -41,19 +42,40 @@ public class LocationModel {
         });
     }
 
+    /**
+     * Adds a location to the Location Model
+     * @param item the location tp be added to the model
+     */
     public void add(Location item) {
         locations.add(item);
         db.collection("location-data").add(item.toMap());
     }
 
+    /**
+     * Gets a list of all locations
+     * @return A list of all locations
+     */
     public List<Location> getLocations() {
         return locations;
     }
 
+    /**
+     * The current location being looked at
+     * @return The current location
+     */
     public Location getCurrentLocation() { return _currentLocation;}
 
+    /**
+     * Sets the current location being looked at. Needed for activities
+     * @param location The new current locations
+     */
     public void setCurrentLocation(Location location) { _currentLocation = location; }
 
+    /**
+     * Finds a location, given an ID
+     * @param id The ID of the location to look for
+     * @return The Location, if found. Or else, null.
+     */
     public Location findLocationById(String id) {
         for (Location d : locations) {
             if (d.getKey().equals(id)) return d;
@@ -62,6 +84,11 @@ public class LocationModel {
         return null;
     }
 
+    /**
+     * Looks for a location by name
+     * @param name The name of the location to look for
+     * @return The location, if found. Or else, null
+     */
     public Location findLocationByName(String name) {
         for (Location d : locations) {
             if (d.getName().equals(name)) return d;
